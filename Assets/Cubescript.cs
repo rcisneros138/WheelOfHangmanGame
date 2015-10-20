c7018b9e8db345ad0c975166f2f6c90e5166f705
unknown <rcisneros_be@dev5.devcodecamp.inc>
2015-10-20 11:52:50 -0500

    first commit

---
+++ Assets/Cubescript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cubescript : MonoBehaviour
+{
+    public AnimationCurve animationCurve;
+
+    private bool spinning;
+
+	// Use this for initialization
+	void Start ()
+    {
+        spinning = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0) && !spinning)
+        {
+            StartCoroutine(DoSpin(10.0f, Random.Range(2000, 3000)));
+        }
+        
+        
+        }
+    public IEnumerator DoSpin(float time, float angle)
+    {
+        spinning = true; float timer = 0;
+        float StartAngle = transform.eulerAngles.z;
+        while (timer < time)
+        {
+            float endAngle = animationCurve.Evaluate(timer / time) * angle;
+            transform.eulerAngles = new Vector3(0.0f, 0.0f, (endAngle+StartAngle));
+            timer += Time.deltaTime;
+            yield return 0;
+        }
+        spinning = false;
+    }
+}
